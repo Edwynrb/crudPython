39e2eb30df4511557f60d556a11a724adcd6223d
Edwyn romero <edwynromero3@gmail.com>
2024-01-25 16:42:32 -0500

    Add the second step for the system

--- BD/conexion.py
+++ BD/conexion.py
@@ -8,13 +8,23 @@ class DAO():
         try:
             self.conection=mysql.connector.connect(
                 host="localhost",
-                port=3306
-                user="root"
-                password="202188"
-                db="course"        
+                port=3306,
+                user="root", 
+                password="202188",
+                db="university"       
             )
         except Error as ex:
             print("Error in conection with db")
             
+# Second step, create method to view all courses
 
+def viewsCourses(self):
+    if self.conection.is_connected():
+       try:
+           cursor=self.conection.cursor()
+           cursor.execute("SELECT * FROM courses ORDER BY name ASC")
+           result=cursor.fetchall()
+           return result
+       except Error as ex:
+           print("Error in connection with db")
     
